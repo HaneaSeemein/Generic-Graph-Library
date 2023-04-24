9cea0faa045c7d369a887b13a437ae2a3634ae17
HaneaSeemein <haniya23seemein@gmail.com>
2023-04-24 11:01:42 +0530

    no errors

--- graph.cpp
+++ graph.cpp
@@ -1,40 +1,29 @@
 #include <vector>
 using namespace std;
+// #define graph;
+template <typename T, typename W>
+class Edge;
 
 template <typename T>
-class Node
-{
-   Node(const T data, )
-   {
-      color = 0;
-      data = data;
-   }
-
-private:
-   void check()
-   {
-      checked = true;
-   }
-   void uncheck()
-   {
-      checked = false;
-   }
-
+class Node{
 public:
    int color;
    T data;
    vector<Edge> from_edges;
    bool checked;
+   Node(const T data)
+   {
+      color = 0;
+      this->data = data;
+   }
+   void check(){checked = true;}
+   void uncheck(){checked = false;}
+
 };
 
 template <typename T, typename W>
 class Edge
 {
-private:
-   Node<T> src, dest;
-   W weight;
-   int color;
-
 public:
    Edge(const Node<T> &src, const Node<T> &dest, const W &weight)
    {
@@ -42,51 +31,50 @@ public:
       this->dest = dest;
       this->weight = weight;
       this->color = 0;
-   };
-   T getSource()
-   {
-      return src;
    }
-   const;
-   T getDestination()
-   {
-      return dest;
-   }
-   const;
-   W getWeight() const;
+
+   Node<T> getSource() const{return src;}
+
+   Node<T> getDestination() const{return dest;}
+
+   W getWeight() const{return weight;}
+private:
+   Node<T> src, dest;
+   W weight;
+   int color;
 };
 
 template <typename T, typename W>
 class Graph
 {
-
 public:
-   Graph(bool directed);
-   Node addNode(const T &data)
+   Graph(bool directed) {}
+
+   Node<T> addNode(const T &data)
    {
-      Node newnode = Node<T>(data);
-   };
+      Node<T> newnode = Node<T>(data);
+      return newnode;
+   }
+
    void addEdge(const Node<T> &src, const Node<T> &dest, const W &weight = W())
    {
-      Edge newEdge = Edge(Node & src, Node & dest, const W &weight = W());
+      Edge<T, W> newEdge = Edge<T, W>(src, dest, weight);
       src.from_edges.push_back(newEdge);
-   };
-   bool hasCycle()
-   {
    }
-   const;
+
+//    bool hasCycle() const {}
+
    // Algorithm implementations
-   vector<int> nodeColoring() const;
-   vector<int> edgeColoring() const;
-   void completeEdges();
-   vector<vector<T>> connectedComponents() const;
-   vector<double> katzCentrality(double alpha, double beta) const;
-   vector<Edge> primMST() const;
-   vector<Edge> kruskalMST() const;
-   vector<T> iterativeDFS(const T &start) const;
-   vector<T> uniformCostSearch(const T &start, const T &goal) const;
-   vector<T> aStarSearch(const T &start, const T &goal, function<double(T, T)> heuristic)
-       const;
+//    vector<int> nodeColoring() const {}
+//    vector<int> edgeColoring() const {}
+//    void completeEdges() {}
+//    vector<vector<T>> connectedComponents() const {}
+//    vector<double> katzCentrality(double alpha, double beta) const {}
+//    vector<Edge<T, W>> primMST() const {}
+//    vector<Edge<T, W>> kruskalMST() const {}
+//    vector<T> iterativeDFS(const T &start) const {}
+//    vector<T> uniformCostSearch(const T &start, const T &goal) const {}
+//    vector<T> aStarSearch(const T &start, const T &goal, function<double(T, T)> heuristic) const {}
 };
 
 // #define later;
--- helper.cpp
+++ helper.cpp
@@ -1,15 +1,1 @@
-#include <vector>
-using namespace std;
-#define graph;
-// template <typename T>
-// void addEdge(const T& src, const T& dest, const W& weight = W()){
 
-// };
-template <typename T>
-void addNode(const T& data){
-
- };
-
-bool hasCycle(){
-    
-}const;
